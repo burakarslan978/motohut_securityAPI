456737e142df913b752b11413901c5ce077f3c0b
Henk post <79155093+henk-post@users.noreply.github.com>
2023-05-19 13:31:57 +0200

    Update FirebaseStorageItem.cs

--- FirebaseStorageItem.cs
+++ FirebaseStorageItem.cs
@@ -2,9 +2,9 @@
 {
     public class FirebaseStorageItem
     {
-        public string Name { get; set; }
+        public string? Name { get; set; }
         public long Size { get; set; }
-        public string ContentType { get; set; }
-        public byte[] Data { get; set; }
+        public string? ContentType { get; set; }
+        public byte[]? Data { get; set; }
     }
 }

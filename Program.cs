d7d364115e0f57a77a9029955f2101ed8b3f40b1
Henk post <79155093+henk-post@users.noreply.github.com>
2023-06-15 15:10:22 +0200

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -23,7 +23,7 @@ builder.Services.AddControllers();
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-builder.Services.AddSingleton<FirebaseStorageService>();
+builder.Services.AddSingleton<IFirebaseStorageService, FirebaseStorageService>();
 
 var app = builder.Build();
 
